d080b4a9269f9fa15c08438b5dbde7b7f75e4554
Suheyb Aden <suheyb1@gmail.com>
2025-11-20 21:30:34 -0800

    Can now build both debug and release builds

--- build.sh
+++ build.sh
@@ -3,7 +3,43 @@
 # Make the build directory if it doesn't already exist
 mkdir -p build
 
-cmake -B ./build
-cmake --build ./build/
+# Defaults to making a debug build
+build_type="Debug"
+
+# Parse the options passed by the user
+while getopts ":r" opt; do
+    case "${opt}" in
+        r) 
+            build_type="Release"
+            ;;
+        \?) echo "ERROR: Invalid option: -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+
+# Verify that there aren't any invalid arguments remaining
+shift $((OPTIND - 1))
+
+if [[ $# -gt 0 ]]; then
+    echo "Error: Unexpected argument '$1'"
+    exit 1
+fi
+
+
+# Let the user know what type of build is occuring
+if [[ $build_type == "Debug" ]]; then
+    echo -e "INFO: Making a debug build!\n"
+elif [[ $build_type == "Release" ]]; then
+    echo -e "INFO: Making a release build!\n"
+else
+    echo "ERROR: Invalid build type - $build_type"
+    exit 1
+fi
+
+
+# Build the program
+cmake -DCMAKE_BUILD_TYPE=$build_type -B ./build
+cmake --build ./build/ --config $build_type
 
 chmod +x ./build/shax-desktop-client
